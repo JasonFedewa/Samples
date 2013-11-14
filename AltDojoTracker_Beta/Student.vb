6c176be7a3f1d7c48bbc6600746323c61e539e17
JasonFedewa <jason.fedewa@yahoo.com>
2013-11-14 07:14:47 -0500

    second commit

--- AltDojoTracker_Beta/Student.vb
+++
@@ -1,5 +0,0 @@
-﻿Partial Public Class Student
-    Public Overrides Function ToString() As String
-        Return "Student: " & Me.LoginName & "  | " & Me.Lname & ", " & Me.Fname & " | " & Me.Rank & " | " & Me.HoursEarned & " | " & "Paid: " & Me.Paid
-    End Function
-End Class
